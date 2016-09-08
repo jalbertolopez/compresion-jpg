2ec1865b8bcf35c0d2aa1b7f8ecb29fd5d034627
J. Alberto Lopez <jalbertolopez10@outlook.com>
2016-09-08 01:49:28 -0500

    Valores de cuantizacion

--- jpg.m
+++ jpg.m
@@ -3,3 +3,13 @@
 function [ imagenComprimida ] = jpg( imagenOriginal , factorCalidad )
 
     [ filas , columnas ] = size( imagenOriginal );
+    
+    if factorCalidad > 50
+        factorCalidad = ( 100 - factorCalidad ) / 50;
+    else
+        if factorCalidad < 50
+            factorCalidad = 50 / factorCalidad;
+        else 
+            factorCalidad = 1;
+        end
+    end
